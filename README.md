eb520f822458afcf38a34ddfc3ac53112469d9b5
CDAC BANGALORE <cdacbangalore404@gmail.com>
2023-03-20 14:39:05 +0530

    Implement the callback function

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Here we will discuss the problem statement by three different method using Async
 1. Callback
 2. Promise
 3. Async/await
+
+documentation link: https://www.freecodecamp.org/news/javascript-async-await-tutorial-learn-callbacks-promises-async-await-by-making-icecream/
---
+++ callback/app.js
@@ -0,0 +1,42 @@
+let stocks = {
+  Fruits: ["strawberry", "grapes", "banana", "apple"],
+  liquid: ["water", "ice"],
+  holder: ["cone", "cup", "stick"],
+  toppings: ["chocolate", "peanuts"]
+};
+
+
+function order(index, callback) {
+    setTimeout(() => {
+        console.log(`${stocks.Fruits[index]} was selected`);
+        callback();
+    }, 2000);
+}
+
+// this is the reason where callback hell is happening here
+function production() {
+    setTimeout(()=>{
+        console.log("Production has started");
+        setTimeout(() => {
+            console.log("The fruits have been chopped");
+            setTimeout(() => {
+                console.log(`Add ${stocks.liquid[0]} and ${stocks.liquid[1]}`);
+                setTimeout(() => {
+                    console.log("The machine has started");
+                    setTimeout(() => {
+                        console.log(`${stocks.holder[0]} was selected`);
+                        setTimeout(() => {
+                            console.log(`${stocks.toppings[0]} was selected`);
+                            setTimeout(() => {
+                                console.log("Ice cream has served");
+                            }, 2000);
+                        }, 3000);
+                    }, 2000);
+                }, 1000)
+            }, 1000);
+        }, 2000);
+    }, 0000);
+}
+
+// calling the function with one callback function
+order(0, production);
